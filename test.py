938b562439d3578438aa4dbdf16c83e25ce3ab75
Vince Kurtz <vjkurtz@gmail.com>
2024-11-04 11:48:10 -0600

    test script moves the target and includes PS

--- test.py
+++ test.py
@@ -5,13 +5,73 @@ import jax
 import jax.numpy as jnp
 import mujoco
 import mujoco.viewer
+import numpy as np
 from hydrax import ROOT
+from hydrax.algs import PredictiveSampling
+from hydrax.tasks.particle import Particle
+from mujoco import mjx
 
 from train import MLP, ScoreMLP
 
 """
 Test a trained model with an interactive sim.
 """
+
+
+def test_predictive_sampling() -> None:
+    """Test the original predictive sampling alg."""
+    # Set up the policy
+    task = Particle()
+    ctrl = PredictiveSampling(task, num_samples=128, noise_level=0.2)
+    policy_params = ctrl.init_params()
+    mjx_data = mjx.make_data(task.model)
+    jit_policy = jax.jit(
+        lambda mjx_data, policy_params: ctrl.optimize(mjx_data, policy_params)[
+            0
+        ],
+        donate_argnames=("policy_params",),
+    )
+
+    # Set up the mujoco simultion
+    xml_path = ROOT + "/models/particle/scene.xml"
+    mj_model = mujoco.MjModel.from_xml_path(xml_path)
+    mj_data = mujoco.MjData(mj_model)
+    mj_data.mocap_pos = np.zeros((1, 3))
+
+    # Set a run time (seconds)
+    run_time = 10
+    total_cost = 0.0
+
+    # Run the simulation
+    with mujoco.viewer.launch_passive(mj_model, mj_data) as viewer:
+        while viewer.is_running() and mj_data.time < run_time:
+            st = time.time()
+
+            # Set the target position
+            x = 0.2 * jnp.cos(1 * mj_data.time)
+            y = 0.2 * jnp.sin(2 * mj_data.time)
+            mj_data.mocap_pos[0, :] = np.array([x, y, 0])
+
+            mjx_data = mjx_data.replace(
+                qpos=jnp.array(mj_data.qpos),
+                qvel=jnp.array(mj_data.qvel),
+                mocap_pos=jnp.array(mj_data.mocap_pos),
+            )
+            policy_params = jit_policy(mjx_data, policy_params)
+
+            mj_data.ctrl[:] = ctrl.get_action(policy_params, 0.0)
+
+            mujoco.mj_step(mj_model, mj_data)
+            viewer.sync()
+
+            elapsed = time.time() - st
+            if elapsed < mj_model.opt.timestep:
+                time.sleep(mj_model.opt.timestep - elapsed)
+
+            pos = mj_data.qpos[:2] - mj_data.mocap_pos[0, :2]
+            total_cost += jnp.linalg.norm(pos)
+
+    print(f"Total cost: {total_cost:.2f}")
 
 
 def test_simple_policy() -> None:
@@ -30,10 +90,19 @@ def test_simple_policy() -> None:
     mj_model = mujoco.MjModel.from_xml_path(xml_path)
     mj_data = mujoco.MjData(mj_model)
 
+    # Set a run time (seconds)
+    run_time = 10
+    total_cost = 0.0
+
     # Run the simulation
     with mujoco.viewer.launch_passive(mj_model, mj_data) as viewer:
-        while viewer.is_running():
+        while viewer.is_running() and mj_data.time < run_time:
             st = time.time()
+
+            # Set the target position
+            x = 0.2 * jnp.cos(1 * mj_data.time)
+            y = 0.2 * jnp.sin(2 * mj_data.time)
+            mj_data.mocap_pos[0, :] = np.array([x, y, 0])
 
             pos = mj_data.qpos[:2] - mj_data.mocap_pos[0, :2]
             obs = jnp.array([pos, mj_data.qvel]).flatten()
@@ -45,6 +114,10 @@ def test_simple_policy() -> None:
             elapsed = time.time() - st
             if elapsed < mj_model.opt.timestep:
                 time.sleep(mj_model.opt.timestep - elapsed)
+
+            total_cost += jnp.linalg.norm(pos)
+
+    print(f"Total cost: {total_cost:.2f}")
 
 
 def test_gpc_policy() -> None:
@@ -69,10 +142,19 @@ def test_gpc_policy() -> None:
     # Initialize the action sequence
     actions = jnp.zeros((1, 5, 2))
 
+    # Set a run time (seconds)
+    run_time = 10
+    total_cost = 0.0
+
     # Run the simulation
     with mujoco.viewer.launch_passive(mj_model, mj_data) as viewer:
-        while viewer.is_running():
+        while viewer.is_running() and mj_data.time < run_time:
             st = time.time()
+
+            # Set the target position
+            x = 0.2 * jnp.cos(1 * mj_data.time)
+            y = 0.2 * jnp.sin(2 * mj_data.time)
+            mj_data.mocap_pos[0, :] = np.array([x, y, 0])
 
             pos = mj_data.qpos[:2] - mj_data.mocap_pos[0, :2]
             obs = jnp.array([pos, mj_data.qvel]).flatten()[None]
@@ -87,7 +169,12 @@ def test_gpc_policy() -> None:
             if elapsed < mj_model.opt.timestep:
                 time.sleep(mj_model.opt.timestep - elapsed)
 
+            total_cost += jnp.linalg.norm(pos)
+
+    print(f"Total cost: {total_cost:.2f}")
+
 
 if __name__ == "__main__":
+    test_predictive_sampling()
     # test_simple_policy()
-    test_gpc_policy()
+    # test_gpc_policy()
