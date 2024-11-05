d4a2c559d18c5bcb16fdd0b1b845c0d7c96ec873
Vince Kurtz <vjkurtz@gmail.com>
2024-11-05 10:01:33 -0600

    implement general data collection

---
+++ gpc/data.py
@@ -0,0 +1,113 @@
+from typing import Callable, Tuple
+
+import jax
+import jax.numpy as jnp
+from flax.struct import dataclass
+from hydrax.alg_base import SamplingBasedController
+from hydrax.task_base import Task
+from mujoco import mjx
+
+"""
+Tools for collecting data by running closed-loop simulations.
+"""
+
+ResetFn = Callable[[int, jax.Array], jax.Array]
+
+
+@dataclass
+class TrainingData:
+    """Container for training data collected during simulation.
+
+    Attributes:
+        observation: The current observation.
+        old_action_sequence: The previous action sequence.
+        new_action_sequence: The updated action sequence.
+    """
+
+    observation: jax.Array
+    old_action_sequence: jax.Array
+    new_action_sequence: jax.Array
+
+
+def collect_data(
+    task: Task,
+    ctrl: SamplingBasedController,
+    num_timesteps: int,
+    num_resets: int,
+    reset_fn: Callable[[mjx.Data, jax.Array], mjx.Data],
+    rng: jax.Array,
+) -> TrainingData:
+    """Collect training data by running closed-loop simulations.
+
+    Args:
+        task: The task to simulate.
+        ctrl: The controller to use for data collection.
+        num_timesteps: The number of timesteps to simulate per rollout.
+        num_resets: The number of rollouts to collect from different states.
+        reset_fn: The function to reset the simulator to a new initial state.
+        rng: The random number generator key.
+
+    Returns:
+        The collected training data, shape (num_resets, num_timesteps, ...).
+    """
+    policy_params = ctrl.init_params()
+    jit_reset = jax.jit(reset_fn, donate_argnums=(0,))
+
+    # Set up the simulator (which matches the controller's model exactly)
+    mjx_model = task.model
+    mjx_data = mjx.make_data(mjx_model)
+
+    def _get_action_sequence(policy_params: jax.Array) -> jax.Array:
+        """Get the action sequence from the controller."""
+        timesteps = jnp.arange(task.planning_horizon) * task.dt
+        return jax.vmap(ctrl.get_action, in_axes=(None, 0))(
+            policy_params, timesteps
+        )
+
+    def _collect_one_rollout(
+        rng: jax.Array, mjx_data: mjx.Data
+    ) -> TrainingData:
+        """Collect data from one rollout."""
+        rng, reset_rng = jax.random.split(rng)
+        mjx_data = jit_reset(mjx_data, reset_rng)
+
+        def _step(carry: Tuple, t: int) -> Tuple[Tuple, TrainingData]:
+            """Simulate one step of the controller."""
+            policy_params, mjx_data = carry
+
+            # Update the policy parameters
+            old_actions = _get_action_sequence(policy_params)
+            policy_params, rollouts = ctrl.optimize(mjx_data, policy_params)
+            new_actions = _get_action_sequence(policy_params)
+
+            # Observations are sorted by randomization, rollout, time
+            obs = rollouts.observations[0, 0, 0]
+
+            # Apply the first control action
+            u = ctrl.get_action(policy_params, 0.0)
+            mjx_data = mjx.step(mjx_model, mjx_data.replace(ctrl=u))
+
+            return (policy_params, mjx_data), TrainingData(
+                observation=obs,
+                old_action_sequence=old_actions,
+                new_action_sequence=new_actions,
+            )
+
+        _, data = jax.lax.scan(
+            _step, (policy_params, mjx_data), jnp.arange(num_timesteps)
+        )
+        return data
+
+    reset_rngs = jax.random.split(rng, num_resets)
+    data = jax.vmap(_collect_one_rollout, in_axes=(0, None))(
+        reset_rngs, mjx_data
+    )
+    return data
+
+
+def visualize_data(
+    task: Task,
+    data: TrainingData,
+) -> None:
+    """Play back the training data on the mujoco visualizer."""
+    pass
---
+++ tests/test_data.py
@@ -0,0 +1,46 @@
+import jax
+from hydrax.algs import PredictiveSampling
+from hydrax.tasks.particle import Particle
+from mujoco import mjx
+
+from gpc.data import collect_data
+
+
+def test_collect_data() -> None:
+    """Test basic data collection."""
+    rng = jax.random.key(0)
+    num_timesteps = 12
+    num_resets = 4
+
+    task = Particle()
+    ctrl = PredictiveSampling(task, num_samples=8, noise_level=0.5)
+
+    def _reset_fn(mjx_data: mjx.Data, rng: jax.Array) -> mjx.Data:
+        """Sample a random initial state for the particle."""
+        rng, pos_rng, vel_rng = jax.random.split(rng, 3)
+        qpos = jax.random.uniform(pos_rng, (2,), minval=-0.29, maxval=0.29)
+        qvel = jax.random.uniform(vel_rng, (2,), minval=-0.5, maxval=0.5)
+        return mjx_data.replace(qpos=qpos, qvel=qvel)
+
+    rng, reset_rng = jax.random.split(rng)
+    dataset = collect_data(
+        task, ctrl, num_timesteps, num_resets, _reset_fn, reset_rng
+    )
+
+    assert dataset.observation.shape == (num_resets, num_timesteps, 4)
+    assert dataset.old_action_sequence.shape == (
+        num_resets,
+        num_timesteps,
+        5,
+        2,
+    )
+    assert dataset.new_action_sequence.shape == (
+        num_resets,
+        num_timesteps,
+        5,
+        2,
+    )
+
+
+if __name__ == "__main__":
+    test_collect_data()
