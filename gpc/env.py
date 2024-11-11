f7caab42c256279a8e06727f034b81eaf6a1f218
Vince Kurtz <vjkurtz@gmail.com>
2024-11-11 10:07:14 -0600

    set up a training env

---
+++ gpc/env.py
@@ -0,0 +1,79 @@
+from abc import ABC, abstractmethod
+
+import jax
+from flax.struct import dataclass
+from hydrax.task_base import Task
+from mujoco import mjx
+
+
+@dataclass
+class SimulatorState:
+    """A dataclass for storing the simulator state.
+
+    Attributes:
+        data: The mjx simulator data.
+        t: The current time step.
+        rng: The random number generator key.
+    """
+
+    data: mjx.Data
+    t: int
+    rng: jax.Array
+
+
+class TrainingEnv(ABC):
+    """Abstract class defining a training environment."""
+
+    def __init__(self, task: Task, episode_length: int) -> None:
+        """Initialize the training environment."""
+        self.task = task
+        self.episode_length = episode_length
+
+    def init_state(self, rng: jax.Array) -> SimulatorState:
+        """Initialize the simulator state."""
+        state = SimulatorState(
+            data=mjx.make_data(self.task.model), t=0, rng=rng
+        )
+        return self._reset_state(state)
+
+    @abstractmethod
+    def reset(self, data: mjx.Data, rng: jax.Array) -> mjx.Data:
+        """Reset the simulator to start a new episode."""
+
+    def _reset_state(self, state: SimulatorState) -> SimulatorState:
+        """Reset the simulator state to start a new episode."""
+        rng, reset_rng = jax.random.split(state.rng)
+        data = self.reset(state.data, reset_rng)
+        return SimulatorState(data=data, t=0, rng=rng)
+
+    def get_observation(self, state: SimulatorState) -> jax.Array:
+        """Get the observation from the simulator state."""
+        # TODO: consider using an abstract class rather than task.get_obs
+        return self.task.get_obs(state.data)
+
+    def episode_over(self, state: SimulatorState) -> bool:
+        """Check if the episode is over.
+
+        Override this method if the episode should terminate early.
+        """
+        return state.t >= self.episode_length
+
+    def step(self, state: SimulatorState, action: jax.Array) -> SimulatorState:
+        """Take a simulation step.
+
+        Args:
+            state: The simulator state.
+            action: The action to take.
+
+        Returns:
+            The new simulator state and the new time step.
+        """
+        return jax.lax.cond(
+            self.episode_over(state),
+            lambda _: self._reset_state(state),
+            lambda _: state.replace(
+                data=mjx.step(self.task.model, state.data.replace(ctrl=action)),
+                t=state.t + 1,
+            ),
+            operand=None,
+        )
