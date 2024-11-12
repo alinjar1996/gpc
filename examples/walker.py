170db96babc97974cc733b1598c91b1f25e58a89
Vince Kurtz <vjkurtz@gmail.com>
2024-11-11 22:11:56 -0600

    add walker example - performs poorly

---
+++ examples/walker.py
@@ -0,0 +1,49 @@
+import sys
+
+from hydrax.algs import PredictiveSampling
+
+from gpc.architectures import ActionSequenceMLP
+from gpc.envs import WalkerEnv
+from gpc.policy import Policy
+from gpc.testing import test_interactive
+from gpc.training import train
+
+if __name__ == "__main__":
+    usage = f"Usage: python {sys.argv[0]} [train|test]"
+
+    if len(sys.argv) != 2:
+        print(usage)
+        sys.exit(1)
+
+    env = WalkerEnv(episode_length=100)
+    save_file = "/tmp/walker_policy.pkl"
+
+    if sys.argv[1] == "train":
+        # Train the policy and save it to a file
+        ctrl = PredictiveSampling(env.task, num_samples=128, noise_level=0.5)
+        net = ActionSequenceMLP(
+            [128, 128], env.task.planning_horizon, env.task.model.nu
+        )
+        policy = train(
+            env,
+            ctrl,
+            net,
+            log_dir="/tmp/gpc_walker",
+            num_iters=10,
+            num_envs=128,
+            batch_size=512,
+            num_epochs=20,
+            learning_rate=1e-3,
+        )
+        policy.save(save_file)
+        print(f"Saved policy to {save_file}")
+
+    elif sys.argv[1] == "test":
+        # Load the policy from a file and test it interactively
+        print(f"Loading policy from {save_file}")
+        policy = Policy.load(save_file)
+        test_interactive(env, policy)
+
+    else:
+        print(usage)
+        sys.exit(1)
