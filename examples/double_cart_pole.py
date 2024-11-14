45cf3fa9b86fc1538ea0fd86ca7edab5259a0c78
Vince Kurtz <vjkurtz@gmail.com>
2024-11-14 10:27:21 -0600

    add double pendulum example, not very performant

---
+++ examples/double_cart_pole.py
@@ -0,0 +1,46 @@
+import sys
+
+from hydrax.algs import PredictiveSampling
+
+from gpc.architectures import DenoisingMLP
+from gpc.envs import DoubleCartPoleEnv
+from gpc.policy import Policy
+from gpc.testing import test_interactive
+from gpc.training import train
+
+if __name__ == "__main__":
+    usage = f"Usage: python {sys.argv[0]} [train|test]"
+
+    if len(sys.argv) != 2:
+        print(usage)
+        sys.exit(1)
+
+    env = DoubleCartPoleEnv(episode_length=200)
+    save_file = "/tmp/double_cart_pole_policy.pkl"
+
+    if sys.argv[1] == "train":
+        # Train the policy and save it to a file
+        ctrl = PredictiveSampling(env.task, num_samples=32, noise_level=0.1)
+        net = DenoisingMLP([64, 64])
+        policy = train(
+            env,
+            ctrl,
+            net,
+            num_policy_samples=16,
+            log_dir="/tmp/gpc_double_cart_pole",
+            num_iters=10,
+            num_envs=128,
+            num_epochs=10,
+        )
+        policy.save(save_file)
+        print(f"Saved policy to {save_file}")
+
+    elif sys.argv[1] == "test":
+        # Load the policy from a file and test it interactively
+        print(f"Loading policy from {save_file}")
+        policy = Policy.load(save_file)
+        test_interactive(env, policy)
+
+    else:
+        print(usage)
+        sys.exit(1)
--- gpc/envs.py
+++ gpc/envs.py
@@ -5,6 +5,7 @@ import jax.numpy as jnp
 from flax.struct import dataclass
 from hydrax.task_base import Task
 from hydrax.tasks.cart_pole import CartPole
+from hydrax.tasks.double_cart_pole import DoubleCartPole
 from hydrax.tasks.particle import Particle
 from hydrax.tasks.pendulum import Pendulum
 from hydrax.tasks.walker import Walker
@@ -180,6 +181,46 @@ class CartPoleEnv(TrainingEnv):
     def observation_size(self) -> int:
         """The size of the observation space (includes sin and cos)."""
         return 5
+
+
+class DoubleCartPoleEnv(TrainingEnv):
+    """Training environment for the double cart-pole swingup task."""
+
+    def __init__(self, episode_length: int) -> None:
+        """Set up the training environment."""
+        super().__init__(task=DoubleCartPole(), episode_length=episode_length)
+
+    def reset(self, data: mjx.Data, rng: jax.Array) -> mjx.Data:
+        """Reset the simulator to start a new episode."""
+        rng, theta_rng, pos_rng, vel_rng = jax.random.split(rng, 4)
+
+        thetas = jax.random.uniform(theta_rng, (2), minval=-3.14, maxval=3.14)
+        pos = jax.random.uniform(pos_rng, (), minval=-2.8, maxval=2.8)
+        qvel = jax.random.uniform(vel_rng, (3,), minval=-2.0, maxval=2.0)
+        qpos = jnp.array([pos, thetas[0], thetas[1]])
+
+        return data.replace(qpos=qpos, qvel=qvel)
+
+    def get_obs(self, data: mjx.Data) -> jax.Array:
+        """Observe the velocity and sin/cos of the angles."""
+        p = data.qpos[0]
+        theta1 = data.qpos[1]
+        theta2 = data.qpos[2]
+        q_obs = jnp.array(
+            [
+                p,
+                jnp.cos(theta1),
+                jnp.sin(theta1),
+                jnp.cos(theta2),
+                jnp.sin(theta2),
+            ]
+        )
+        return jnp.concatenate([q_obs, data.qvel])
+
+    @property
+    def observation_size(self) -> int:
+        """The size of the observation space (includes sin and cos)."""
+        return 8
 
 
 class WalkerEnv(TrainingEnv):
