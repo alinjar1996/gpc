f3df9b48b6ada750cd7116783e7ce71aca2beb94
Vince Kurtz <vjkurtz@gmail.com>
2024-11-18 12:57:45 -0600

    save and restore example

--- tests/test_architectures.py
+++ tests/test_architectures.py
@@ -50,38 +50,37 @@ def test_mlp_construction() -> None:
         p.unlink()
     local_dir.rmdir()
 
+
 def test_mlp_save_load() -> None:
     """Verify that we can pickle an MLP."""
-    rng = jax.random.key(0)
-    mlp = MLP(layer_sizes=(2, 3, 4))
-    dummy_input = jnp.ones((3,))
-    params = mlp.init(rng, dummy_input)
-
-    original_output = mlp.apply(params, dummy_input)
+    layer_sizes = [2, 3, 4]
+    mlp = MLP(layer_sizes, rngs=nnx.Rngs(1))
+    dummy_input = jnp.ones((2,))
+    original_output = mlp(dummy_input)
 
     # Create a temporary path for saving stuff
     local_dir = Path("_test_mlp")
     local_dir.mkdir(parents=True, exist_ok=True)
 
-    # Save the MLP
+    _, state = nnx.split(mlp)
+
+    # Save the model parameters
     model_path = local_dir / "mlp.pkl"
     with Path(model_path).open("wb") as f:
-        pickle.dump(mlp, f)
+        pickle.dump(state, f)
 
-    # Save the parameters (weights)
-    params_path = local_dir / "params.pkl"
-    with Path(params_path).open("wb") as f:
-        pickle.dump(params, f)
+    # Load the model from a file
+    abstract_model = nnx.eval_shape(lambda: MLP(layer_sizes, rngs=nnx.Rngs(0)))
+    graphdef, _ = nnx.split(abstract_model)
 
-    # Load the MLP and parameters
     with Path(model_path).open("rb") as f:
-        new_mlp = pickle.load(f)
-    with Path(params_path).open("rb") as f:
-        new_params = pickle.load(f)
+        state_restored = pickle.load(f)
 
-    # Check that the loaded MLP gives the same output
-    new_output = new_mlp.apply(new_params, dummy_input)
-    assert jnp.allclose(original_output, new_output)
+    model_restored = nnx.merge(graphdef, state_restored)
+
+    # Check that the model is still functional
+    restored_output = model_restored(dummy_input)
+    assert jnp.allclose(original_output, restored_output)
 
     # Remove the temporary directory
     for p in local_dir.iterdir():
@@ -138,7 +137,7 @@ def test_denoising_mlp() -> None:
 
 
 if __name__ == "__main__":
-    test_mlp_construction()
-    # test_mlp_save_load()
+    # test_mlp_construction()
+    test_mlp_save_load()
     # test_action_sequence_mlp()
     # test_denoising_mlp()
