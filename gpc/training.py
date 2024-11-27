e341a61104f9f495753b12d3608abc2130a7cd07
Vince Kurtz <vjkurtz@gmail.com>
2024-11-27 11:38:22 -0600

    [WIP] try a uniform proposal distribution

--- gpc/training.py
+++ gpc/training.py
@@ -150,7 +150,10 @@ def fit_policy(
 
         # Sample noise and time steps for the flow matching targets
         rng, noise_rng, t_rng = jax.random.split(rng, 3)
-        noise = jax.random.normal(noise_rng, batch_act.shape)
+        # noise = jax.random.normal(noise_rng, batch_act.shape)
+        noise = jax.random.uniform(
+            rng, batch_act.shape, minval=-1.0, maxval=1.0
+        )
         t = jax.random.uniform(t_rng, (batch_size, 1))
 
         # Compute the loss and its gradient
