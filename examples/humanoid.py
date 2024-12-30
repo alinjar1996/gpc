5503a44fe33201db52327959d804fe6f2b92b7d3
Vince Kurtz <vjkurtz@gmail.com>
2024-12-30 09:12:49 -0800

    add basic humanoid example

---
+++ examples/humanoid.py
@@ -0,0 +1,57 @@
+import sys
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+
+from flax import nnx
+from hydrax.algs import PredictiveSampling
+
+from gpc.architectures import DenoisingCNN
+from gpc.envs import HumanoidEnv
+from gpc.policy import Policy
+from gpc.testing import test_interactive
+from gpc.training import train
+
+if __name__ == "__main__":
+    usage = f"Usage: python {sys.argv[0]} [train|test]"
+
+    if len(sys.argv) != 2:
+        print(usage)
+        sys.exit(1)
+
+    env = HumanoidEnv(episode_length=200)
+    save_file = "/tmp/humanoid_policy.pkl"
+
+    if sys.argv[1] == "train":
+        # Train the policy and save it to a file
+        ctrl = PredictiveSampling(env.task, num_samples=128, noise_level=1.0)
+        net = DenoisingCNN(
+            action_size=env.task.model.nu,
+            observation_size=env.observation_size,
+            horizon=env.task.planning_horizon,
+            feature_dims=(64,) * 5,
+            rngs=nnx.Rngs(0),
+        )
+        policy = train(
+            env,
+            ctrl,
+            net,
+            num_policy_samples=32,
+            log_dir="/tmp/gpc_humanoid",
+            num_iters=500,
+            num_envs=128,
+            num_epochs=5,
+            checkpoint_every=2,
+        )
+        policy.save(save_file)
+        print(f"Saved policy to {save_file}")
+
+    elif sys.argv[1] == "test":
+        # Load the policy from a file and test it interactively
+        print(f"Loading policy from {save_file}")
+        policy = Policy.load(save_file)
+        test_interactive(env, policy, sim_timestep=0.01)
+
+    else:
+        print(usage)
+        sys.exit(1)
