a9934c32ce7e05537757ac686cb858f4534e3a66
Vince Kurtz <vjkurtz@gmail.com>
2025-01-11 10:33:00 -0600

    update pusht

--- examples/pusht.py
+++ examples/pusht.py
@@ -1,26 +1,37 @@
-import sys
+import argparse
 
 import mujoco
 from flax import nnx
 from hydrax.algs import PredictiveSampling
+from hydrax.simulation.deterministic import run_interactive as run_sampling
 
 from gpc.architectures import DenoisingCNN
 from gpc.envs import PushTEnv
 from gpc.policy import Policy
+from gpc.sampling import BootstrappedPredictiveSampling
 from gpc.testing import test_interactive
 from gpc.training import train
 
 if __name__ == "__main__":
-    usage = f"Usage: python {sys.argv[0]} [train|test]"
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description="Push a T-shaped block on a table"
+    )
+    subparsers = parser.add_subparsers(
+        dest="task", help="What to do (choose one)"
+    )
+    subparsers.add_parser("train", help="Train (and save) a generative policy")
+    subparsers.add_parser("test", help="Test a generative policy")
+    subparsers.add_parser(
+        "sample", help="Bootstrap sampling-based MPC with a generative policy"
+    )
+    args = parser.parse_args()
 
-    if len(sys.argv) != 2:
-        print(usage)
-        sys.exit(1)
-
+    # Set up the environment and save file
     env = PushTEnv(episode_length=400)
     save_file = "/tmp/pusht_policy.pkl"
 
-    if sys.argv[1] == "train":
+    if args.task == "train":
         # Train the policy and save it to a file
         ctrl = PredictiveSampling(env.task, num_samples=128, noise_level=0.4)
         net = DenoisingCNN(
@@ -44,7 +55,7 @@ if __name__ == "__main__":
         policy.save(save_file)
         print(f"Saved policy to {save_file}")
 
-    elif sys.argv[1] == "test":
+    elif args.task == "test":
         # Load the policy from a file and test it interactively
         print(f"Loading policy from {save_file}")
         policy = Policy.load(save_file)
@@ -52,6 +63,21 @@ if __name__ == "__main__":
         mj_data.qpos[:] = [0.1, 0.1, 1.5, 0.0, 0.0]  # set the initial state
         test_interactive(env, policy, mj_data)
 
+    elif args.task == "sample":
+        # Use the policy to bootstrap sampling-based MPC
+        policy = Policy.load(save_file)
+        ctrl = BootstrappedPredictiveSampling(
+            policy,
+            env.get_obs,
+            warm_start_level=0.5,
+            num_policy_samples=32,
+            task=env.task,
+            num_samples=1,
+            noise_level=0.1,
+        )
+        mj_model = env.task.mj_model
+        mj_data = mujoco.MjData(mj_model)
+        run_sampling(ctrl, mj_model, mj_data, frequency=50)
+
     else:
-        print(usage)
-        sys.exit(1)
+        parser.print_help()
