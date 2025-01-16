beada8ac745dc09377974719b270e09ea2092a57
Vince Kurtz <vjkurtz@gmail.com>
2025-01-16 12:25:07 -0600

    Add luffing crane example (#12)
    * draft crane example (reduced DoFs)
    
    * basic crane example works from narrow set of initial conditions
    
    * randomize initial configurations
    
    * clean and document some quirks

---
+++ examples/crane.py
@@ -0,0 +1,85 @@
+import argparse
+
+import mujoco
+from flax import nnx
+from hydrax.algs import PredictiveSampling
+from hydrax.simulation.deterministic import run_interactive as run_sampling
+
+from gpc.architectures import DenoisingCNN
+from gpc.envs import CraneEnv
+from gpc.policy import Policy
+from gpc.sampling import BootstrappedPredictiveSampling
+from gpc.testing import test_interactive
+from gpc.training import train
+
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description="Swing the payload of a luffing crane to a target position."
+    )
+    subparsers = parser.add_subparsers(
+        dest="task", help="What to do (choose one)"
+    )
+    subparsers.add_parser("train", help="Train (and save) a generative policy")
+    subparsers.add_parser("test", help="Test a generative policy")
+    subparsers.add_parser(
+        "sample", help="Bootstrap sampling-based MPC with a generative policy"
+    )
+    args = parser.parse_args()
+
+    # Set up the environment and save file
+    env = CraneEnv(episode_length=500)
+    save_file = "/tmp/crane_policy.pkl"
+
+    if args.task == "train":
+        # Train the policy and save it to a file
+        ctrl = PredictiveSampling(env.task, num_samples=4, noise_level=0.05)
+        net = DenoisingCNN(
+            action_size=env.task.model.nu,
+            observation_size=env.observation_size,
+            horizon=env.task.planning_horizon,
+            feature_dims=[64, 64],
+            timestep_embedding_dim=16,
+            rngs=nnx.Rngs(0),
+        )
+        policy = train(
+            env,
+            ctrl,
+            net,
+            num_policy_samples=2,
+            log_dir="/tmp/gpc_crane",
+            num_iters=10,
+            num_envs=512,
+            num_videos=4,
+            batch_size=1024,
+            num_epochs=20,
+            checkpoint_every=5,
+            strategy="policy",
+        )
+        policy.save(save_file)
+        print(f"Saved policy to {save_file}")
+
+    elif args.task == "test":
+        # Load the policy from a file and test it interactively
+        print(f"Loading policy from {save_file}")
+        policy = Policy.load(save_file)
+        test_interactive(env, policy)
+
+    elif args.task == "sample":
+        # Use the policy to bootstrap sampling-based MPC
+        policy = Policy.load(save_file)
+        ctrl = BootstrappedPredictiveSampling(
+            policy,
+            env.get_obs,
+            inference_timestep=0.1,
+            num_policy_samples=4,  # samples from the flow-matching policy
+            task=env.task,
+            num_samples=4,  # samples from a gaussian around the previous mean
+            noise_level=0.05,
+        )
+        mj_model = env.task.mj_model
+        mj_data = mujoco.MjData(mj_model)
+        run_sampling(ctrl, mj_model, mj_data, frequency=30)
+
+    else:
+        parser.print_help()
--- gpc/envs.py
+++ gpc/envs.py
@@ -7,6 +7,7 @@ import numpy as np
 from flax.struct import dataclass
 from hydrax.task_base import Task
 from hydrax.tasks.cart_pole import CartPole
+from hydrax.tasks.crane import Crane
 from hydrax.tasks.cube import CubeRotation
 from hydrax.tasks.double_cart_pole import DoubleCartPole
 from hydrax.tasks.humanoid import Humanoid
@@ -512,3 +513,74 @@ class CubeEnv(TrainingEnv):
     def observation_size(self) -> int:
         """The size of the observation space."""
         return 48
+
+
+class CraneEnv(TrainingEnv):
+    """Training environment for the luffing crane end-effetor tracking task."""
+
+    def __init__(self, episode_length: int = 100) -> None:
+        """Set up the particle training environment."""
+        super().__init__(task=Crane(), episode_length=episode_length)
+
+    def reset(self, data: mjx.Data, rng: jax.Array) -> mjx.Data:
+        """Reset the simulator to start a new episode."""
+        rng, pos_rng, vel_rng, target_rng = jax.random.split(rng, 4)
+
+        # Crane state
+        # TODO: figure out a more principled way to initialize these.
+        # Right now the crane swings wildly at initialization, which prevents
+        # gathering a lot of data near the target (which is where the system is
+        # mostly at run time).
+        q_lim = jnp.array(
+            [
+                [-1.0, 1.0],  # slew
+                [0.0, 1.0],  # luff
+                [-1.0, 1.0],  # payload x-pos
+                [1.0, 2.2],  # payload y-pos
+                [0.3, 1.0],  # payload z-pos
+                [1.0, 1.0],  # payload orientation (fixed upright)
+                [0.0, 0.0],
+                [0.0, 0.0],
+                [0.0, 0.0],
+            ]
+        )
+        qpos = self.task.model.qpos0 + jax.random.uniform(
+            pos_rng,
+            (self.task.model.nq,),
+            minval=q_lim[:, 0],
+            maxval=q_lim[:, 1],
+        )
+        qvel = jax.random.uniform(
+            vel_rng, (self.task.model.nv,), minval=-0.1, maxval=0.1
+        )
+
+        # Target position
+        # TODO: figure out a better set of potential target positions
+        pos_min = jnp.array([-1.5, 1.0, 0.0])
+        pos_max = jnp.array([1.5, 3.0, 1.5])
+        target_pos = jax.random.uniform(
+            target_rng, (3,), minval=pos_min, maxval=pos_max
+        )
+        mocap_pos = data.mocap_pos.at[0].set(target_pos)
+
+        # Target orientation - this is unused but must be set so vectorization
+        # (which is determined by the size of rng) works properly.
+        target_quat = jnp.array([1.0, 0.0, 0.0, 0.0]) + jax.random.uniform(
+            target_rng, (4,), minval=-0.0, maxval=0.0
+        )
+        mocap_quat = data.mocap_quat.at[0].set(target_quat)
+
+        return data.replace(
+            qpos=qpos, qvel=qvel, mocap_pos=mocap_pos, mocap_quat=mocap_quat
+        )
+
+    def get_obs(self, data: mjx.Data) -> jax.Array:
+        """Observe the full crane state, plus end-effector pos/vel."""
+        ee_pos = self.task._get_payload_position(data)
+        ee_vel = self.task._get_payload_velocity(data)
+        return jnp.concatenate([ee_pos, ee_vel, data.qpos, data.qvel])
+
+    @property
+    def observation_size(self) -> int:
+        """The size of the observation space."""
+        return 23
